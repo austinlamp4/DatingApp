fa02f06e21c2b599192be511593443277c33788e
Christopher Lamp <austinlamp4@gmail.com>
2023-08-08 07:24:09 -0600

    Adding Angular front-end for .NET API to be used
    Added an Angular front-end application to call the .NET API, primarily a skeleton with some configuration to run over HTTPS and to have the .NET API use CORS to allow Angular to reach said API.

--- API/Program.cs
+++ API/Program.cs
@@ -11,8 +11,11 @@ builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")); //Uses builder.Configuration.GetConnectionString to get the connection string from the Config file
 }); //Adding our DB service, opt is specifying our options
 
+builder.Services.AddCors();
 var app = builder.Build();
 
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 //Middleware to map the controllers
 app.MapControllers();
 
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DatingApp
+This application is built using an ASP.NET API with intentions of an Angular front-end to practice writing code, enhance my understanding of web applications.
