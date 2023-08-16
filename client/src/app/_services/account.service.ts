af8f187d81d7592c9397688e022b822d169cc3d9
Christopher Lamp <austinlamp4@gmail.com>
2023-08-16 13:15:33 -0600

    Added persistent login via Client-Side changes
    Using observables and some changes client-side, login is now persistent and the JWT token is stored in the Browser's localStorage component. Changes were made to use the observable in a safe manner with an Asynchronous pipe and establish persistent access as a client after login.

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -1,13 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
+  
 export class AccountService {
   baseUrl = 'https://localhost:5000/api/';
+  private currentUserSource = new BehaviorSubject<User | null>(null); //This observable can be User or null
+  currentUser$ = this.currentUserSource.asObservable(); //$ indicates this is an observable
+
   constructor(private http: HttpClient) { }
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) { //Will be used from component to set the information inside AccountService
+    this.currentUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
