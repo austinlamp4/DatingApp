638e6b55341b2e2e2e751fa9646c3a822126b77a
Christopher Lamp <austinlamp4@gmail.com>
2023-09-07 08:12:30 -0600

    Photo Upload functionality created
    Photo upload functionality was built in to the client side app and connected to the API. The API was extended to store photos to cloudinary using a free storage account and pull the photo URL back and return it to the client-side app to allow for display of the photo after successful upload.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,8 +1,11 @@
 using System.Security.Claims;
 using API.DTOs;
+using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -12,10 +15,12 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository userRepository;
     private readonly IMapper mapper;
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    private readonly IPhotoService photoService;
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
         this.userRepository = userRepository;
         this.mapper = mapper;
+        this.photoService = photoService;
     }
 
     [HttpGet] //Creating a GET request to get all users, defaults to /api/users since that's what's configured at the [Route] and we didn't specify anything additional like below.
@@ -33,7 +38,7 @@ public class UsersController : BaseApiController
     [HttpPut]
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value; //Get the username from the token
+        var username = User.GetUsername(); //Get the username from the token
         var user = await this.userRepository.GetUserByUsernameAsync(username);
 
         if (user == null) return NotFound();
@@ -43,5 +48,33 @@ public class UsersController : BaseApiController
         if (await this.userRepository.SaveAllAsync()) return NoContent();
 
         return BadRequest("Failed to update the user");
+    }
+
+    [HttpPost("add-photo")]
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file) 
+    {
+        var user = await this.userRepository.GetUserByUsernameAsync(User.GetUsername());
+        if (user == null) return NotFound(); //Defensive code to make sure that we have a user before continuing
+
+        var result = await this.photoService.AddPhotoAsync(file);
+
+        if (result.Error != null) return BadRequest(result.Error.Message); //Defensive code to ensure that we didn't receive an error before continuing
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        if (user.Photos.Count == 0) photo.IsMain = true; //If this is their first photo upload, make it their main photo by default
+
+        user.Photos.Add(photo);
+
+        if (await this.userRepository.SaveAllAsync())
+        {
+            return CreatedAtAction(nameof(GetUser), new {username = user.UserName}, this.mapper.Map<PhotoDto>(photo));
+        } 
+
+        return BadRequest("Problem adding photo"); //If we make it to this point then there was a problem saving the photo.
     }
 }
